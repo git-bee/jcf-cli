81672e0aac266e0116079f3c21d6959d4b058e6d
pakLebah <pak.lebah@yahoo.com>
2018-06-30 15:23:52 +0700

    fix bug on single line mode

--- Process/Returns/BlockStyles.pas
+++ Process/Returns/BlockStyles.pas
@@ -2,13 +2,13 @@ unit BlockStyles;
 
 {(*}
 (*------------------------------------------------------------------------------
- Delphi Code formatter source code 
+ Delphi Code formatter source code
 
 The Original Code is BlockStyles, released May 2003.
-The Initial Developer of the Original Code is Anthony Steele. 
+The Initial Developer of the Original Code is Anthony Steele.
 Portions created by Anthony Steele are Copyright (C) 1999-2008 Anthony Steele.
-All Rights Reserved. 
-Contributor(s): Anthony Steele. 
+All Rights Reserved.
+Contributor(s): Anthony Steele.
 
 The contents of this file are subject to the Mozilla Public License Version 1.1
 (the "License"). you may not use this file except in compliance with the License.
@@ -16,11 +16,11 @@ You may obtain a copy of the License at http://www.mozilla.org/NPL/
 
 Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied.
-See the License for the specific language governing rights and limitations 
+See the License for the specific language governing rights and limitations
 under the License.
 
 Alternatively, the contents of this file may be used under the terms of
-the GNU General Public License Version 2 or later (the "GPL") 
+the GNU General Public License Version 2 or later (the "GPL")
 See http://www.gnu.org/licenses/gpl.html
 ------------------------------------------------------------------------------*)
 {*)}
@@ -161,15 +161,24 @@ begin
        }
       Result := FormatSettings.Returns.ElseIfStyle;
     end
-    else if (lcNextToken.TokenType = ttBegin) then
+    else if (lcNextToken.TokenType = ttBegin) or (lcNextToken.TokenType = ttTry) or
+       (lcNextToken.TokenType = ttRepeat) or (lcNextToken.TokenType = ttCase) or
+       (lcNextToken.TokenType = ttWhile) or (lcNextToken.TokenType = ttWith) or
+       (lcNextToken.TokenType = ttIf) or (lcNextToken.TokenType = ttFor) then
     begin
-      Result := FormatSettings.Returns.ElseBeginStyle;
-    end;
+      // Result := FormatSettings.Returns.ElseBeginStyle;
+      Result := FormatSettings.Returns.BlockBeginStyle;
+    end
+    else
+      Result := FormatSettings.Returns.BlockStyle;
   end
   else
   begin
     { otherwise, is there a begin next? }
-    if lcNextToken.TokenType = ttBegin then
+    if (lcNextToken.TokenType = ttBegin) or (lcNextToken.TokenType = ttTry) or
+       (lcNextToken.TokenType = ttRepeat) or (lcNextToken.TokenType = ttCase) or
+       (lcNextToken.TokenType = ttWhile) or (lcNextToken.TokenType = ttWith) or
+       (lcNextToken.TokenType = ttIf) or (lcNextToken.TokenType = ttFor) then
       Result := FormatSettings.Returns.BlockBeginStyle
     else
       Result := FormatSettings.Returns.BlockStyle;
