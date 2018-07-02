e5af0208f5270641d32fc63ba0fcaecc6537977d
pakLebah <pak.lebah@yahoo.com>
2018-07-02 17:16:47 +0700

    Add erroneous test.pas file for testing.

--- test.pas
+++ test.pas
@@ -2,31 +2,33 @@ program test;
 
 (* JCF CLI test file *)
 
-uses
-  CRT;
+uses CRT;
 
 procedure myProcedure;
-var
-  s: string;
+var s: String;
 begin
   s := 'this is a string';
-  writeLn(s);
+  writeln(s);
 end;
 
 function myFunction(aParam: string): boolean;
-var
-  i: integer;
+var i: integer;
 begin
   for i := 1 to 10 do
     if i < 10 then write(i, ',')
-    else writeLn(i);
+      else writeln(i);
 
   i := 1;
   repeat
     case i of
-      1..9: for i := 1 to i do write(i, ',');
-      10: writeLn;
-      else myProcedure;
+      1..9:
+        for i := 1 to i do write(i, ',');
+      10: begin
+        writeln;
+        writeln;
+      end;
+      else
+        myProcedure;
     end;
   until i >= 10;
 
@@ -35,29 +37,30 @@ end;
 
 //# main program
 var
-  c, r: integer;
-  s: string;
-  a: array of integer;
+  c, r: Integer;
+  s: String;
+  a: Array of Integer;
 
-begin
+BEGIN
   //! clear first
   ClrScr;
 
   SetLength(a, 10);
-  for c := 0 to High(a) do a[c] := c;
-  for c := 0 to High(a) do
-    if i < High(a) then write(a[c], ',')
-    else writeLn;
+  FOR c := 0 TO High(a) DO
+    a[c] := c;
+  FOR c := 0 TO High(a) DO
+    IF i < High(a) THEN write(a[c], ',')
+      ELSE writeln;
 
   c := ScreenWidth; //* screen size
   r := ScreenHeight;
   s := 'Screen size: ';
-  writeLn(s, c, '×', r);
+  writeln(s, c, '×', r);
 
   write('Enter your name: '); //? input
-  readLn(s);
-  writeLn('Hello, ', s, '!'); //+ forgotten
+  readln(s);
+  writeln('Hello, ', s, '!'); //+ forgotten
 
   //-readln; // unnecessary
   // TODO: to-do next
-end.
+END.
